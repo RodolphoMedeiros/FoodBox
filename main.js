67671f094144e104bafb15b4c40a4c401b03cf33
Rodolpho Medeiros <rodolphomedeiroos93@gmail.com>
2022-04-27 21:27:40 -0300

    modificação no js e no gitignore

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function pratoPrincipal1(){
         pratoPrincipal.push("Carne com molho da casa") ;
     }else{
         document.querySelectorAll(".container")[0].classList.remove("container-chosen1");
-        pratoPrincipal.splice("Carne com molho da casa");
+        pratoPrincipal.splice(pratoPrincipal.indexOf("Carne com molho da casa"), 1);
     }
 }
 function pratoPrincipal2(){
@@ -19,7 +19,7 @@ function pratoPrincipal2(){
         pratoPrincipal.push("Peixe frito com um pouco de salada"); 
     }else{
         document.querySelectorAll(".container")[1].classList.remove("container-chosen1");
-        pratoPrincipal.splice("Peixe frito com um pouco de salada");
+        pratoPrincipal.splice(pratoPrincipal.indexOf("Peixe frito com um pouco de salada"), 1);
     }
 }
 function pratoPrincipal3(){
@@ -28,7 +28,7 @@ function pratoPrincipal3(){
         pratoPrincipal.push("Salada com tempero árabe"); 
     }else{
         document.querySelectorAll(".container")[2].classList.remove("container-chosen1");
-        pratoPrincipal.splice("Salada com tempero árabe");
+        pratoPrincipal.splice(pratoPrincipal.indexOf("Salada com tempero árabe"), 1);
     }
 }
 
@@ -46,6 +46,7 @@ function bebidas1(){
         bebidas.push("Coca-cola"); 
     }else{
         document.querySelectorAll(".container")[3].classList.remove("container-chosen1");
+        bebidas.splice(bebidas.indexOf("Coca-cola"), 1);
     }
 }
 function bebidas2(){
@@ -54,6 +55,7 @@ function bebidas2(){
         bebidas.push("Resfresco"); 
     }else{
         document.querySelectorAll(".container")[4].classList.remove("container-chosen1");
+        bebidas.splice(bebidas.indexOf("Resfresco"), 1);
     }
 }
 function bebidas3(){
@@ -62,6 +64,7 @@ function bebidas3(){
         bebidas.push("Água Mineral"); 
     }else{
         document.querySelectorAll(".container")[5].classList.remove("container-chosen1");
+        bebidas.splice(bebidas.indexOf("Água Mineral"), 1);
     }
 }
 
@@ -76,6 +79,7 @@ function sobremesa1(){
         sobremesas.push("Pudim"); 
     }else{
         document.querySelectorAll(".container")[6].classList.remove("container-chosen1");
+        sobremesas.splice(sobremesas.indexOf("Pudim"), 1);
     }
 }
 function sobremesa2(){
@@ -84,6 +88,7 @@ function sobremesa2(){
         sobremesas.push("Bolo de chocolate"); 
     }else{
         document.querySelectorAll(".container")[7].classList.remove("container-chosen1");
+        sobremesas.splice(sobremesas.indexOf("Bolo de chocolate"), 1);
     }
 }
 function sobremesa3(){
@@ -92,6 +97,7 @@ function sobremesa3(){
         sobremesas.push("Sorvete"); 
     }else{
         document.querySelectorAll(".container")[8].classList.remove("container-chosen1");
+        sobremesas.splice(sobremesas.indexOf("Sorvete"), 1);
     }
 }
 document.querySelectorAll(".container")[6].addEventListener("click", sobremesa1);
@@ -99,15 +105,16 @@ document.querySelectorAll(".container")[7].addEventListener("click", sobremesa2)
 document.querySelectorAll(".container")[8].addEventListener("click", sobremesa3);
 
 function button(){
-  // como pular linha dentro do confirm??  
-    if( window.confirm((`Seu pedido foi:\n Prato(s): ${ pratoPrincipal.join("; ")} \n Bebida(s): ${bebidas.join("; ")}\n Sobremesa(s):  ${sobremesas.join("; ")}`   )) == true){
-        let mensagem = `Olá, gostaria de pedir um combo com:\n ${pratoPrincipal.join("; ")} \n${bebidas.join("; ")} \n ${sobremesas.join("; ")}`;
-        window.open("https://wa.me/+5521993534009?text=" + mensagem);
-        
-
+    if( pratoPrincipal.length == "" && bebidas.length == "" && sobremesas.length == "") {
+        document.querySelector(".button").style.disable="disable";
+        alert("É necessário a escolha de pelo menos 1(um) item para dar continuidade ao pedido!")
+    }else{
+        if( window.confirm((`Seu pedido foi:\n Prato(s): ${ pratoPrincipal.join("; ")} \n Bebida(s): ${bebidas.join("; ")}\n Sobremesa(s):  ${sobremesas.join("; ")}`   )) == true){
+            
+            let mensagem = `Olá, gostaria de pedir um combo com:\n ${pratoPrincipal.join("; ")} \n${bebidas.join("; ")} \n ${sobremesas.join("; ")}`;
+            window.open("https://wa.me/+5521999999999?text=" + mensagem);
+        }
     }
-    
-   
 }
 document.querySelector(".button").addEventListener("click", button);
 
